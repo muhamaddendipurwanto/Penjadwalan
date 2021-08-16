04e2a6ee1713e220079811dce6cff6192fbaa0b8
unknown <hendarpriana@gmail.com>
2021-08-16 15:08:24 +0700

    Upload SS dan Database

---
+++ DATABASE/pembelajaran.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 16, 2021 at 10:04 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pembelajaran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_guru`
+--
+
+CREATE TABLE `data_guru` (
+  `kode` int(11) NOT NULL,
+  `NIP` varchar(40) DEFAULT NULL,
+  `Nama_guru` varchar(50) DEFAULT NULL,
+  `Alamat_Tinggal` varchar(250) DEFAULT NULL,
+  `HP` varchar(15) DEFAULT NULL,
+  `Status` char(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_jadwal`
+--
+
+CREATE TABLE `data_jadwal` (
+  `No` int(11) NOT NULL,
+  `Kode_jd` varchar(7) DEFAULT NULL,
+  `Kd_Guru` varchar(8) DEFAULT NULL,
+  `Nama_Guru` varchar(50) DEFAULT NULL,
+  `Mata_Pelajaran` varchar(20) DEFAULT NULL,
+  `Kelas` varchar(10) DEFAULT NULL,
+  `ID_hari` int(11) DEFAULT NULL,
+  `Jam` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_mapel`
+--
+
+CREATE TABLE `data_mapel` (
+  `No` int(11) NOT NULL,
+  `Kode` int(11) DEFAULT NULL,
+  `Nama_Pelajaran` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `No` int(11) NOT NULL,
+  `NIS` varchar(20) DEFAULT NULL,
+  `Nama` varchar(50) DEFAULT NULL,
+  `Kelas` varchar(15) DEFAULT NULL,
+  `JK` char(2) DEFAULT NULL,
+  `TL` varchar(150) DEFAULT NULL,
+  `Agama` varchar(30) DEFAULT NULL,
+  `Hp` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_guru`
+--
+ALTER TABLE `data_guru`
+  ADD PRIMARY KEY (`kode`);
+
+--
+-- Indexes for table `data_jadwal`
+--
+ALTER TABLE `data_jadwal`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `data_mapel`
+--
+ALTER TABLE `data_mapel`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_guru`
+--
+ALTER TABLE `data_guru`
+  MODIFY `kode` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `data_jadwal`
+--
+ALTER TABLE `data_jadwal`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `data_mapel`
+--
+ALTER TABLE `data_mapel`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
